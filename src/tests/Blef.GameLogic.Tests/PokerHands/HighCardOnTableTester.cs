44caaf3605258048a58c61eb7d44f275fe060dc0
Artur Wincenciak <teo.vincent.com@gmail.com>
2017-03-01 00:28:21 +0100

    move subtype of test methods to two diferent classes

--- src/tests/Blef.GameLogic.Tests/PokerHands/HighCardOnTableTester.cs
+++ src/tests/Blef.GameLogic.Tests/PokerHands/HighCardOnTableTester.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace Blef.GameLogic.Tests.PokerHands
 {
-    public class HighCardTests
+    public class HighCardOnTableTester
     {
         [Fact]
         public void Given_table_without_high_card_Then_result_is_False()
@@ -45,17 +45,6 @@ namespace Blef.GameLogic.Tests.PokerHands
             bool isOnTable = highCard.IsOnTable(cardsOnTable);
 
             Assert.True(isOnTable);
-        }
-
-        [Fact]
-        public void When_comparing_better_hand_to_worse_hand_then_result_is_bigger_than_zero()
-        {
-            HighCard betterHand = new HighCard(new Card(Rank.Jack, Suit.Diamonds));
-            HighCard worseHand = new HighCard(new Card(Rank.Nine, Suit.Diamonds));
-
-            int result = betterHand.CompareTo(worseHand);
-
-            Assert.True(result > 0);
-        }
+        } 
     }
 }
---
+++ src/tests/Blef.GameLogic.Tests/PokerHands/HightCardComaringTester.cs
@@ -0,0 +1,19 @@
+﻿using Blef.GameLogic.PokerHands;
+using Xunit;
+
+namespace Blef.GameLogic.Tests.PokerHands
+{
+    public class HightCardComaringTester
+    {
+        [Fact]
+        public void When_comparing_better_hand_to_worse_hand_then_result_is_bigger_than_zero()
+        {
+            HighCard betterHand = new HighCard(new Card(Rank.Jack, Suit.Diamonds));
+            HighCard worseHand = new HighCard(new Card(Rank.Nine, Suit.Diamonds));
+
+            int result = betterHand.CompareTo(worseHand);
+
+            Assert.True(result > 0);
+        }
+    }
+}
