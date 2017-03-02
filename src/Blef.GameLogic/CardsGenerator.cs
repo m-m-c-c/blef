e7f7337b1272fdb40daf3943b8550d005a3ea1b0
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-02 21:46:53 +0100

    Add CardsGenerator

---
+++ src/Blef.GameLogic/CardsGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Blef.GameLogic
+{
+    public class CardsGenerator
+    {
+        public static Rank[] GetAllRanks()
+        {
+            return new[]
+            {
+                Rank.Nine,
+                Rank.Ten,
+                Rank.Jack,
+                Rank.Queen,
+                Rank.King,
+                Rank.Ace
+            };
+        }
+    }
+}
