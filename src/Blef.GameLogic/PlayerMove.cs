59b820c674bce968e3e61305b6dd735f2118a19b
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-02 21:52:50 +0100

    Add factory methods for PlayerMove

--- src/Blef.GameLogic/PlayerMove.cs
+++ src/Blef.GameLogic/PlayerMove.cs
@@ -6,5 +6,26 @@ namespace Blef.GameLogic
     {
         public PlayerChoice Choice { get; set; }
         public PokerHand PokerHand { get; set; }
+
+        public static PlayerMove Check()
+        {
+            return new PlayerMove
+            {
+                Choice = PlayerChoice.Check
+            };
+        }
+
+        public static PlayerMove Bet(PokerHand pokerHand)
+        {
+            return new PlayerMove
+            {
+                Choice = PlayerChoice.Bet,
+                PokerHand = pokerHand
+            };
+        }
+
+        PlayerMove()
+        {
+        }
     }
 }
