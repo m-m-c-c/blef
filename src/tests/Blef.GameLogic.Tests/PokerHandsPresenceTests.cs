fdf17d3c8e5dbcc555412b3903befb89a19bc6ba
Cezary Piątek <cepi@ailleron.com>
2017-03-03 22:32:12 +0100

    Raplace xUnit with NUnit

--- src/tests/Blef.GameLogic.Tests/PokerHandsPresenceTests.cs
+++ src/tests/Blef.GameLogic.Tests/PokerHandsPresenceTests.cs
@@ -1,22 +1,14 @@
 ﻿using System.Collections.Generic;
 using Blef.GameLogic.PokerHands;
-using Xunit;
+using NUnit.Framework;
 
 namespace Blef.GameLogic.Tests
 {
     public class PokerHandsPresenceTests
     {
-        public static IReadOnlyList<Rank> AllRanks = new[]
-            {Rank.Nine, Rank.Ten, Rank.Jack, Rank.Queen, Rank.King, Rank.Ace};
-
-        [Theory]
-        [InlineData(Rank.Nine)]
-        [InlineData(Rank.Ten)]
-        [InlineData(Rank.Jack)]
-        [InlineData(Rank.Queen)]
-        [InlineData(Rank.King)]
-        [InlineData(Rank.Ace)]
-        public void should_be_able_to_tell_that_highcard_is_not_presence_when_table_is_empty(Rank highCardRank)
+        [Test]
+        public void should_be_able_to_tell_that_highcard_is_not_presence_when_table_is_empty(
+            [Values(Rank.Nine, Rank.Ten, Rank.Jack, Rank.Queen, Rank.King, Rank.Ace)]Rank highCardRank)
         {
             //ARRANGE
             var table = new Table
@@ -38,18 +30,11 @@ namespace Blef.GameLogic.Tests
             Assert.False(isPresence);
         }
 
-        public static IEnumerable<object[]> GenerateAllRankSuitCombinations()
-        {
-            foreach (var suit in new[] {Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades})
-            {
-                foreach (var rank in AllRanks)
-                    yield return new object[] { rank, suit };
-            }
-        }
-
-        [Theory]
-        [MemberData(nameof(GenerateAllRankSuitCombinations))]
-        public void should_be_able_to_tell_that_highcard_is_presence_when_there_is_a_single_card_on_the_table(Rank highCardRank, Suit suit)
+       [Test]
+       [Pairwise]
+        public void should_be_able_to_tell_that_highcard_is_presence_when_there_is_a_single_card_on_the_table(
+           [Values(Rank.Nine, Rank.Ten, Rank.Jack, Rank.Queen, Rank.King, Rank.Ace)]Rank highCardRank, 
+           [Values(Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades)]Suit suit)
         {
             //ARRANGE
             var table = new Table
