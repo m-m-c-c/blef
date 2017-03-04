8fe8ee8aa30c149692c52da153f39edb7b189840
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-04 11:56:17 +0100

    Test showing bug: TwoPair_Ace_and_Nine_should_be_stronger_than_king_and_queen()

---
+++ src/tests/Blef.GameLogic.Tests/PokerHands/TwoPairHandTests.cs
@@ -0,0 +1,17 @@
+﻿using Blef.GameLogic.PokerHands;
+using Xunit;
+
+namespace Blef.GameLogic.Tests.PokerHands
+{
+    public class TwoPairHandTests
+    {
+        [Fact]
+        public void TwoPair_Ace_and_Nine_should_be_stronger_than_king_and_queen()
+        {
+            TwoPairs A9 = new TwoPairs(Rank.Ace, Rank.Nine);
+            TwoPairs KQ = new TwoPairs(Rank.King, Rank.Queen);
+
+            Assert.True(A9.IsStrongerThan(KQ));
+        }
+    }
+}
