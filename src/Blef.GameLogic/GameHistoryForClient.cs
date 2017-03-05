0f94f406682b428df4d01ef032b48b27fbbab910
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-06 00:18:29 +0100

    Revert "Add GameHistoryForClient class"
    This reverts commit 371770b76a18f1413ca95e525f58d740822c60a4.

--- src/Blef.GameLogic/GameHistoryForClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blef.GameLogic
-{
-    // Name could be better but I don't have a better one now
-    public class GameHistoryForClient
-    {
-        public PlayerBet[] PlayerBets { get; set; }
-        public PlayerHand PlayerHand { get; set; }
-    }
-}
