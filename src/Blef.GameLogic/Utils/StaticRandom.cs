fec2564fb4ea5d5d87880009fd6d79473619c222
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-06 00:05:22 +0100

    Implement Card.DealCard()

---
+++ src/Blef.GameLogic/Utils/StaticRandom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Blef.GameLogic.Utils
+{
+    /// <summary>
+    /// This class is used, cause we don't want to create Random class
+    /// every time. It is possible that 'new Random' that are close in code
+    /// and time will give the same random values.
+    /// </summary>
+    public static class StaticRandom
+    {
+        public static readonly Random Instance = new Random();
+    }
+}
