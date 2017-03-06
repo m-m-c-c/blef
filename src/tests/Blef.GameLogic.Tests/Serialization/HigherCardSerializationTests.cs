1db247c0669e116dacaf51f0027b29eff97f6c4d
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-06 01:19:36 +0100

    Craete HigherCard_should_be_serialized_and_deserialized_correctly_to_the_exact_type() test
    It was not so clear that this is possible to get the same type back
    after JSON serialization. In this test it is show that
    TypeNameHandling.All is doing the job.

---
+++ src/tests/Blef.GameLogic.Tests/Serialization/HigherCardSerializationTests.cs
@@ -0,0 +1,18 @@
+﻿using Blef.GameLogic.PokerHands;
+using Xunit;
+
+namespace Blef.GameLogic.Tests.Serialization
+{
+    public class HigherCardSerializationTests
+    {
+        [Fact]
+        public void HigherCard_should_be_serialized_and_deserialized_correctly_to_the_exact_type()
+        {
+            var pokerHand = new HighCard(Rank.Ace);
+
+            var deserializedObject = SerializationTestHelper.SerializeAndDeserialize(pokerHand);
+
+            Assert.IsType<HighCard>(deserializedObject);
+        }
+    }
+}
---
+++ src/tests/Blef.GameLogic.Tests/Serialization/SerializationTestHelper.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Blef.GameLogic.Tests.Serialization
+{
+    public class SerializationTestHelper
+    {
+        public static T SerializeAndDeserialize<T>(T value)
+        {
+            JsonSerializerSettings jss = new JsonSerializerSettings
+            {
+                // It is important for serializing PokerHand derived types
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+            string serialized = JsonConvert.SerializeObject(value, jss);
+
+            return JsonConvert.DeserializeObject<T>(serialized, jss);
+        }
+    }
+}
