997f84a6eeaf6a4f9e04930b9d6f19d5c7ee06a1
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-06 09:09:34 +0100

    Add PokerHand description with link to Blef wiki

--- src/Blef.GameLogic/PokerHands/PokerHand.cs
+++ src/Blef.GameLogic/PokerHands/PokerHand.cs
@@ -2,6 +2,10 @@
 
 namespace Blef.GameLogic.PokerHands
 {
+    /// <summary>
+    /// Ranks should be assigned according to
+    /// <see href="https://pl.wikipedia.org/wiki/Blef_(gra)"/>
+    /// </summary>
     public abstract class PokerHand
     {
         public abstract bool IsOnTable(Table table);
