fd1bf990052d47aaca2a85a3f4d99a402ef5fc39
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-07 19:21:33 +0100

    Add test DeckTests.Can_deal_maximum_24_cards_from_deck()

---
+++ src/tests/Blef.GameLogic.Tests/DeckTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Blef.GameLogic.Tests
+{
+    public class DeckTests
+    {
+        [Fact]
+        public void Can_deal_maximum_24_cards_from_deck()
+        {
+            var fullDeck = Deck.GetFullDeck();
+
+            for (int i = 0; i < 24; i++)
+            {
+                fullDeck.DealCard();
+            }
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                fullDeck.DealCard();
+            });
+        }
+    }
+}
