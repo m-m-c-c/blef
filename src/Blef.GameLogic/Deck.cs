a62a9040cc5a03100788ae05718f8d17cf7d92a8
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-07 18:56:58 +0100

    Create Deck and deck creation

---
+++ src/Blef.GameLogic/Deck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blef.GameLogic.Utils;
+
+namespace Blef.GameLogic
+{
+    public class Deck
+    {
+        readonly List<Card> _cards;
+
+        public static Deck GetFullDeck()
+        {
+            IReadOnlyCollection<Card> readOnlyCollection = DeckCardsGenerator.GetStartingDeck();
+
+            var fullDeck = new Deck(readOnlyCollection.ToList());
+
+            return fullDeck;
+        }
+
+        Deck(List<Card> cards)
+        {
+            _cards = cards;
+        }
+
+        public Card DealCard()
+        {
+            if (_cards.Count <= 0)
+            {
+                throw new InvalidOperationException("Cannot deal more cards from deck. Deck is empty");
+            }
+
+            Random random = StaticRandom.Instance;
+
+            int randomPosition = random.Next(0, _cards.Count);
+
+            Card card = _cards[randomPosition];
+
+            _cards.RemoveAt(randomPosition);
+
+            return card;
+        }
+    }
+}
