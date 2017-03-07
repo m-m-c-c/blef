fc73fa20c128078a8f3874f194ab7a9014d7d6ac
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-03-07 18:56:58 +0100

    Create Deck and deck creation

---
+++ src/Blef.GameLogic/DeckCardsGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Blef.GameLogic
+{
+    internal static class DeckCardsGenerator
+    {
+        /// <summary>
+        /// This list is readonly so it can be reused by all Decks (<see cref="Deck"/>).
+        /// <see cref="Card"/> is immutable and it is enough to have only one instance
+        /// of each Card in the whole system.
+        /// </summary>
+        public static IReadOnlyCollection<Card> GetStartingDeck()
+        {
+            var cards = new List<Card>(24);
+
+            foreach (var rank in CardsGenerator.GetAllRanks())
+            {
+                foreach (var suit in CardsGenerator.GetAllSuites())
+                {
+                    cards.Add(new Card(rank, suit));
+                }
+            }
+
+            return cards.AsReadOnly();
+        }
+    }
+}
